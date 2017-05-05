db0f8b881e36c4f841958a23f0ba4052311d2546
Administrator <976997161@qq.com>
2017-05-05 09:42:20 +0800

    freemaker添加

--- diich-core/src/main/java/com/diich/core/model/SecUser.java
+++ diich-core/src/main/java/com/diich/core/model/SecUser.java
@@ -12,7 +12,6 @@ public class SecUser extends BaseModel{
 
     private String password;
 
-    private Date create_time;
 
     public Integer getUser_id() {
         return user_id;
@@ -38,11 +37,5 @@ public class SecUser extends BaseModel{
         this.password = password == null ? null : password.trim();
     }
 
-    public Date getCreate_time() {
-        return create_time;
-    }
 
-    public void setCreate_time(Date create_time) {
-        this.create_time = create_time;
-    }
 }
