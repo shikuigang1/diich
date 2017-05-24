a9de386d3635929503e88894e2c179514afc1183
shikuigang <slting12@163.com>
2017-05-24 09:32:24 +0800

    Merge branch 'master' of D:\workspace\diich with conflicts.

---
+++ diich-web-biz/src/main/java/com/diich/controller/SysUserController.java
@@ -0,0 +1,62 @@
+package com.diich.controller;
+
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.plugins.Page;
+import com.diich.core.model.SecUser;
+import com.diich.core.service.SysUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/4/24 0024.
+ */
+@Controller
+@RequestMapping(value="/sys")
+public class SysUserController {
+
+
+
+    @ResponseBody
+    @RequestMapping("/page")
+    public Object selectPage(Model model){
+
+        Page page=new Page(1,10);
+
+        EntityWrapper ew=new EntityWrapper();
+        ew.setEntity(new SecUser());
+
+        Integer id=1;
+        ew.where("id> {0}",1).orderBy("id");
+
+        List<SecUser>  ls= null;//sysUserService.selectUserPage(page, ew);
+
+        System.out.println(ls.size());
+        return ls;
+    }
+
+    /**
+     * session 测试
+     */
+    @RequestMapping(value="/set")
+    @ResponseBody
+    public void setSession(HttpServletRequest request,String str){
+        request.getSession().setAttribute("str",str);
+    }
+
+    @RequestMapping(value="/test")
+    public String testSession(HttpServletRequest request,String str,Model model){
+        Object str1 =  request.getSession().getAttribute("str");
+
+        System.out.println(request.getSession().getId());
+        System.out.println(request.getRemoteHost()+":"+str1);
+
+        model.addAttribute("str",str1);
+        return "index";
+    }
+}
