acaba0097b21c66ad57f326edde680611ec1bd68
shikuigang <slting12@163.com>
2017-06-01 18:20:24 +0800

    111

--- diich-core/src/main/java/com/diich/core/service/IchProjectService.java
+++ diich-core/src/main/java/com/diich/core/service/IchProjectService.java
@@ -24,4 +24,7 @@ public interface IchProjectService {
     IchProject getIchProjectById(Long id) throws Exception;
 
     String buildHTML(String templateName, IchProject ichProject,String fileName) throws Exception;
+
+    //项目创建 搜索使用  最多显示 5 条数据
+    List<IchProject> getIchProjectByName(Map<String,Object> map) throws Exception;
 }
