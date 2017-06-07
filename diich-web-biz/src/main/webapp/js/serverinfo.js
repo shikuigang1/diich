159a3fdb04247a4726c156cd3b8962d2ef907392
shikuigang <slting12@163.com>
2017-06-07 09:36:13 +0800

    111

---
+++ diich-web-biz/src/main/webapp/js/serverinfo.js
@@ -0,0 +1,5 @@
+var serverPath = 'http://localhost';//默认服务地址
+var defaultheadimg = 'http://192.168.1.111/headsmall.png';//默认头像地址
+var imgserver = 'http://diich-resource.oss-cn-beijing.aliyuncs.com';//默认头像地址
+
+
