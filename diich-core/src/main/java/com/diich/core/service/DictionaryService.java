879d874f9e681bc839e178c39fb184bc62810cf8
xiaoyong <916648803@qq.com>
2017-06-13 14:33:48 +0800

    update

--- diich-core/src/main/java/com/diich/core/service/DictionaryService.java
+++ diich-core/src/main/java/com/diich/core/service/DictionaryService.java
@@ -8,9 +8,9 @@ import java.util.List;
  * Created by Administrator on 2017/5/22.
  */
 public interface DictionaryService {
-    List<Dictionary> getDictionaryListByType(Integer type) throws Exception;
+    List<Dictionary> getDictionaryListByType(Integer type, String language) throws Exception;
 
-    String getTextByTypeAndCode(Integer type, String code) throws Exception;
+    String getTextByTypeAndCode(Integer type, String code, String language) throws Exception;
 
     List<Dictionary> getAllDictionary() throws Exception;
 
