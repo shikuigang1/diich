427a9c4bf0caa99f04a90ae4b91d1979793031fd
xiaoyong <916648803@qq.com>
2017-06-14 16:50:00 +0800

    修改dictionary接口

--- diich-web-biz/src/main/webapp/js/util.js
+++ diich-web-biz/src/main/webapp/js/util.js
@@ -1,16 +1,59 @@
+/**
+ * 通过type和code获取字典文本
+ * @param type 类型
+ * @param code 字典编码
+ * @param lang 语言，默认为中文（不传lang参数）
+ * @returns {string}
+ */
+function getTextByTypeAndCode(type, code, lang) {
+    if(typeof dic_arr == 'undefined') {
+        alert('请引入dictionary.js文件');
+        return;
+    }
 
-function getTextByTypeAndCode(type, code) {
+    if(typeof lang == 'undefined') {
+        lang = 'chi';
+    }
+
     var text = '';
 
     for(var i = 0; i < dic_arr.length; i ++) {
         var dic_obj = dic_arr[i];
-        if(dic_obj.type == type && dic_obj.code == code) {
+        if(dic_obj.type == type && dic_obj.code == code
+                && dic_obj.lang == lang) {
             text = dic_obj.name;
             break;
         }
     }
 
     return text;
+}
+
+/**
+ * 获取同一类型的字典数据
+ * @param type 数据类型
+ * @param lang 语言，默认为中文（不传lang参数）
+ */
+function getDictionaryArrayByType(type, lang) {
+    if(typeof dic_arr == 'undefined') {
+        alert('请引入dictionary.js文件');
+        return;
+    }
+
+    if(typeof lang == 'undefined') {
+        lang = 'chi';
+    }
+
+    var array = [];
+
+    for(var i = 0; i < dic_arr.length; i ++) {
+        var dic_obj = dic_arr[i];
+        if(dic_obj.type == type && dic_obj.lang == lang) {
+            array.push(dic_obj);
+        }
+    }
+
+    return array;
 }
 
 function uploadFile(uri) {
@@ -23,6 +66,18 @@ function uploadFile(uri) {
         },
         error : function(e) {
             alert(e.responseText);
+        }
+    });
+}
+
+function getTemplateUi(fileName) {
+    $.ajax({
+        url: fileName,
+        async: false,
+        contentType: 'text/plain;charset=utf-8',
+        dataType: 'text',
+        success: function(data, status) {
+            alert(typeof data);
         }
     });
 }
