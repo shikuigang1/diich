0f13b3cd7cd14ae37322ddfb3c919cfd8e74f551
Administrator <976997161@qq.com>
2017-07-11 10:51:37 +0800

    简介

--- diich-core/src/main/java/com/diich/core/service/IchCategoryService.java
+++ diich-core/src/main/java/com/diich/core/service/IchCategoryService.java
@@ -11,4 +11,5 @@ import java.util.List;
 public interface IchCategoryService {
     List<IchCategory> getAllCategory() throws Exception;
     IchCategory getCategoryById(Long id) throws Exception;
+    List<Attribute> getAttributeListByCatId(Long id) throws Exception;
 }
