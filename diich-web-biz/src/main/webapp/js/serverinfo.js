e14225a0381206899b6e6711ec20c38e412b1caf
shikuigang <slting12@163.com>
2017-08-04 15:57:23 +0800

    test

--- diich-web-biz/src/main/webapp/js/serverinfo.js
+++ diich-web-biz/src/main/webapp/js/serverinfo.js
@@ -1,5 +1,5 @@
 var serverPath = 'http://localhost';//默认服务地址
-var defaultheadimg = 'http://192.168.1.111/headsmall.png';//默认头像地址
-var imgserver = 'http://diich-resource.oss-cn-beijing.aliyuncs.com';//默认头像地址
+var defaultheadimg = '../assets/images/headsmall.png';//默认头像地址
+var imgserver = 'http://diich-resource.oss-cn-beijing.aliyuncs.com';//默认头像地址http://resource.efeiyi.com/image/
 
 
