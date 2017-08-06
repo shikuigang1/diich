6889c0b2bc4f165c0ff47528da56fa46c6df3d40
shikuigang <slting12@163.com>
2017-08-06 10:44:48 +0800

    test

--- diich-web-biz/src/main/webapp/assets/js/system.js
+++ diich-web-biz/src/main/webapp/assets/js/system.js
@@ -952,3 +952,21 @@ var cloneObj = function (obj) {
     }
     return newObj;
 };
+var tipBox={
+    init:function (type,text,speed) {
+        this.template(type,text,speed);
+    },
+    template:function (type,text,speed) {
+        var html='<div class="tipbox '+type+'" style="display: none;">' +
+            '        <div class="head">提示</div>' +
+            '        <div class="content">' +
+            '            <i class="icon"></i><span>'+text+'</span>' +
+            '        </div>' +
+            '    </div>';
+        $('body').append(html);
+        $('.tipbox').fadeIn(300);
+        setTimeout(function () {
+            $('.tipbox').fadeOut(300).remove();
+        },speed)
+    }
+};
