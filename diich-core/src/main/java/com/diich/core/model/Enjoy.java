81fdc81ae8bc42fced3e9dd1a98aabc9f9304b37
Administrator <976997161@qq.com>
2017-08-30 17:38:31 +0800

    attribute

---
+++ diich-core/src/main/java/com/diich/core/model/Enjoy.java
@@ -0,0 +1,35 @@
+package com.diich.core.model;
+
+import com.diich.core.base.BaseModel;
+
+public class Enjoy extends BaseModel {
+    private Long id;
+
+    private Long targetId;
+
+    private Integer targetType;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getTargetId() {
+        return targetId;
+    }
+
+    public void setTargetId(Long targetId) {
+        this.targetId = targetId;
+    }
+
+    public Integer getTargetType() {
+        return targetType;
+    }
+
+    public void setTargetType(Integer targetType) {
+        this.targetType = targetType;
+    }
+}
