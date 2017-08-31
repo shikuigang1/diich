9881a1d24da221d15fba04cada76eea96d5ce2f5
shikuigang <slting12@163.com>
2017-08-31 15:01:50 +0800

    test

---
+++ diich-web-biz/src/main/webapp/assets/js/common.js
@@ -0,0 +1,13 @@
+document.write("<script type=\"text/javascript\" src=\"../../assets/js/jquery.min.js\"></script>");
+document.write("<script type=\"text/javascript\" src=\"../../assets/js/system.js\"></script>");
+/*document.write("<script type=\"text/javascript\" src=\"../../data/keyword.js\"></script>");*/
+document.write("<script type=\"text/javascript\" src=\"../../js/category.js\"></script>");
+document.write("<script type=\"text/javascript\" src=\"../../data/error_message.js\"></script>");
+document.write("<script type=\"text/javascript\" src=\"../../js/util.js\"></script>");
+document.write("<script type=\"text/javascript\" src=\"../../assets/js/utils.js\"></script>");
+document.write("<script type=\"text/javascript\" src=\"../../js/citys.js\"></script>");
+document.write("<script type=\"text/javascript\" src=\"../../js/jquery.i18n.properties-1.0.9.js\"></script>");
+document.write("<script type=\"text/javascript\" src=\"../../assets/js/login.js\"></script>");
+document.write("<script type=\"text/javascript\" src=\"../../js/i18n.js\"></script>");
+
+
