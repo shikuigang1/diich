f75d17b4cef4a77cfae7a7e91f592101f5a74bb8
xiaoyong <916648803@qq.com>
2017-09-26 14:42:13 +0800

    update

--- diich-web-biz/src/main/webapp/js/inheritor-edit.js
+++ diich-web-biz/src/main/webapp/js/inheritor-edit.js
@@ -6,5 +6,34 @@ $(function () {
 });
 
 function init() {
+    $('.edit.link').on('click', function() {
+        var inheritorId = $(this).attr('inheritor-id');
+        if(inheritorId == null) {
+            alert('获取项目信息失败');
+            return;
+        }
+        loadInheritorFromServer(inheritorId);
+    });
+}
 
+function loadInheritorFromServer(inheritorId) {
+    $.ajax({
+        type: 'POST',
+        url: '../ichMaster/getIchMasterById?params=' + inheritorId,
+        /*xhrFields:{
+            withCredentials:true
+        },*/
+        success: function (data) {
+            if(data == null || data.code == 3) {
+                alert('您还没有登录，请登录后操作！');
+                return;
+            }
+            inheritor = data.data;
+            //loadAttributesFromServer();
+            //displayEditMode();
+        },
+        error: function () {
+
+        }
+    });
 }
