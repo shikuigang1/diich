e8622da9d88795c97709d0ca37c76cd296197b76
xiaoyong <916648803@qq.com>
2017-10-13 11:10:36 +0800

    update

--- diich-web-biz/src/main/webapp/js/base-url.js
+++ diich-web-biz/src/main/webapp/js/base-url.js
@@ -1,3 +1,3 @@
-var base_url = 'http://diich.efeiyi.com';
-var PROJECT_RESOURCE_URL = 'http://project.efeiyi.com/image/project/';
-var MASTER_RESOURCE_URL = 'http://inheritor.efeiyi.com/image/master';
+var base_url = 'http://www.diich.com';
+var PROJECT_RESOURCE_URL = 'http://project.diich.com/image/project/';
+var MASTER_RESOURCE_URL = 'http://inheritor.diich.com/image/master/';
