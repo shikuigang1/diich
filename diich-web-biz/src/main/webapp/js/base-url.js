997b1f8425361bfde8a29486a63b78f6e7f46737
xiaoyong <916648803@qq.com>
2018-01-18 14:02:58 +0800

    update

--- diich-web-biz/src/main/webapp/js/base-url.js
+++ diich-web-biz/src/main/webapp/js/base-url.js
@@ -1,4 +1,4 @@
-var base_url = 'http://www.diich.com';
-//var base_url = 'http://47.95.32.236';
+//var base_url = 'http://www.diich.com';
+var base_url = 'http://47.95.32.236';
 var PROJECT_RESOURCE_URL = 'http://project.diich.com/image/project/';
 var MASTER_RESOURCE_URL = 'http://inheritor.diich.com/image/master/';
